082f07e3d80956bda5b091efb37550056b09ead5
Bülent Dayıoğlu <bulentdayioglu@Bulent-MacBook-Pro.local>
2025-10-29 21:29:26 +0300

    bug fix v2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/bulentdayioglu/', // REPO_ADI: GitHub repo adın
+  base: '/', // REPO_ADI: GitHub repo adın
   plugins: [vue(), tailwindcss()],
 })
